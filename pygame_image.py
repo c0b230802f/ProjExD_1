2da9f7d4477b43b1fe40032491d444e8904b490e
Yusuke Shimizu <c0b230802f@edu.teu.ac.jp>
2024-04-16 17:57:26 +0900

    演習1

--- pygame_image.py
+++ pygame_image.py
@@ -21,6 +21,7 @@ def main():
         for event in pg.event.get():
             if event.type == pg.QUIT: return
         key_lst = pg.key.get_pressed()
+        kk_rct.move_ip([-1, 0])
         # print(key_lst)
         if key_lst[pg.K_UP]:
             # print("上押された")
@@ -30,7 +31,7 @@ def main():
         if key_lst[pg.K_LEFT]:
             kk_rct.move_ip([-1, 0])            
         if key_lst[pg.K_RIGHT]:
-            kk_rct.move_ip([+1, 0])
+            kk_rct.move_ip([+2, 0])
         x = tmr % 3200
         screen.blit(bg_img, [-x, 0])
         screen.blit(bg_imgs, [-x+1600, 0])
