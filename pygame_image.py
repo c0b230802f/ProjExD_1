362ba57724f5f86d4f7ba6df232a567e5ba71ed7
Yusuke Shimizu <c0b230802f@edu.teu.ac.jp>
2024-04-16 17:48:50 +0900

    第8

--- pygame_image.py
+++ pygame_image.py
@@ -10,19 +10,33 @@ def main():
     screen = pg.display.set_mode((800, 600))
     clock  = pg.time.Clock()
     bg_img = pg.image.load("fig/pg_bg.jpg")
-    #bk_img = pg.transform.flip(bg_img, True, False)
+    bg_imgs = pg.transform.flip(bg_img, True, False)
     kt_img = pg.image.load("fig/3.png")
     kt_img = pg.transform.flip(kt_img, True, False)
+    kk_rct = kt_img.get_rect()  # 練習８-１（こうかとんRectを抽出）
+    kk_rct.center =  300, 200  # 練習８-２（中心座標の設定）
     tmr = 0
     
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: return
-
-        x = tmr % 1600
+        key_lst = pg.key.get_pressed()
+        # print(key_lst)
+        if key_lst[pg.K_UP]:
+            # print("上押された")
+            kk_rct.move_ip([0, -1])
+        if key_lst[pg.K_DOWN]:
+            kk_rct.move_ip([0, +1])            
+        if key_lst[pg.K_LEFT]:
+            kk_rct.move_ip([-1, 0])            
+        if key_lst[pg.K_RIGHT]:
+            kk_rct.move_ip([+1, 0])
+        x = tmr % 3200
         screen.blit(bg_img, [-x, 0])
-        #screen.blit(bk_img, [-x+1600, 0])
-        screen.blit(kt_img, [300,200])
+        screen.blit(bg_imgs, [-x+1600, 0])
+        screen.blit(bg_img, [-x+3200, 0])
+        screen.blit(bg_imgs, [-x+4800, 0])
+        screen.blit(kt_img, kk_rct) 
         pg.display.update()
         tmr += 1        
         clock.tick(200)
